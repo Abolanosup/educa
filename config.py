e04f635a3d3df4e8d8283dada239c0dd673be418
ghost0210 <cristianghost4@gmail.com>
2022-12-13 21:38:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 # Bot
-BOT_TOKEN = ''
-TG_API_ID = ''
-TG_API_HASH = ''
-TG_ADMIN = 'obisoftt'
+BOT_TOKEN = '5444581119:AAFTxQF9x0RXHsMA58rXCpCrAYQIrTEDl3A'
+TG_API_ID = '13233271'
+TG_API_HASH = 'e3ce8145aa657c2a4cc5cf0f7183e476'
+TG_ADMIN = 'Ghosthell0210'
 
 # Database
 DB_LOCAL = False
